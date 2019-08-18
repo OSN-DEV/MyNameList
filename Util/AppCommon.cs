d62164a14678371ff9ff0e4e7d3bc93379d13de2
OSN-DEV <osndev1973@gmail.com>
2019-08-18 20:31:15 +0900

    Add Skeleton

---
+++ Util/AppCommon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNameList.Util {
+    /// <summary>
+    /// アプリ汎用メソッド
+    /// </summary>
+    internal class AppCommon {
+
+        #region Public Method
+        /// <summary>
+        /// アプリの実行パスを取得する。
+        /// </summary>
+        /// <returns>アプリの実行パス</returns>
+        internal static string GetAppPath() {
+            return null;
+        }
+
+        /// <summary>
+        /// バージョン付きアプリ名称を取得する。
+        /// </summary>
+        /// <returns>バージョン付きアプリ名称</returns>
+        internal static string GetAppName() {
+            return null;
+        }
+        #endregion
+    }
+}
