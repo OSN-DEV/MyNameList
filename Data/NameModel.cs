7b5c245aaca8bb4cdfdcce9c79f7bf27fc1d4843
OSN-DEV <osndev1973@gmail.com>
2019-08-18 10:01:40 +0900

    名称モデルを追加

---
+++ Data/NameModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNameList.Data {
+    /// <summary>
+    /// 名称モデル。
+    /// </summary>
+    internal class NameModel {
+
+        #region Public Property
+        /// <summary>
+        /// 英名。
+        /// </summary>
+        public string EnglishName { set; get; } = "";
+
+        /// <summary>
+        /// 和名。
+        /// </summary>
+        public string JapaneseName { set; get; } = "";
+
+        /// <summary>
+        /// メモ。
+        /// </summary>
+        public string Note { set; get; } = "";
+        #endregion
+    }
+}
