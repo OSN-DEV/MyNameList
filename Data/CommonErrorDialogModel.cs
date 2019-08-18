a6caadc2ab36c94c8b69385f9c51319da024293b
OSN-DEV <osndev1973@gmail.com>
2019-08-18 13:55:17 +0900

    汎用エラーダイアログのスケルトンを追加

---
+++ Data/CommonErrorDialogModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNameList.Data {
+    /// <summary>
+    /// 汎用エラーダイアログモデル
+    /// </summary>
+    public class CommonErrorDialogModel {
+
+        #region Public Property
+        /// <summary>
+        /// ダイアログのタイトル。省略時は"Error"を設定。
+        /// </summary>
+        public string Title { set; get; } = "Error";
+
+        /// <summary>
+        /// エラーメッセージ
+        /// </summary>
+        public string ErrorMessage { set; get; } = "";
+
+        /// <summary>
+        /// エラーの発生したファイルのフルパス
+        /// </summary>
+        public string FilePath { set; get; } = "";
+        #endregion
+    }
+}
