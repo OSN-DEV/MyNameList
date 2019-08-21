f1fd25ee4c42e262cf148b9a3aa46bb015f86631
OSN-DEV <osndev1973@gmail.com>
2019-08-21 21:56:58 +0900

    汎用メソッドの実装

--- Util/AppCommon.cs
+++ Util/AppCommon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,15 +17,39 @@ namespace MyNameList.Util {
         /// </summary>
         /// <returns>アプリの実行パス</returns>
         internal static string GetAppPath() {
-            return null;
+            var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (path.EndsWith(@"\")) {
+                return path;
+            } else {
+                return path + @"\";
+            }
         }
+
 
         /// <summary>
         /// バージョン付きアプリ名称を取得する。
         /// </summary>
         /// <returns>バージョン付きアプリ名称</returns>
         internal static string GetAppName() {
-            return null;
+            var fullname = typeof(App).Assembly.Location;
+            var info = System.Diagnostics.FileVersionInfo.GetVersionInfo(fullname);
+            var ver = info.FileVersion;
+            return string.Format("MyNameList({0})", ver);
+        }
+
+
+        /// <summary>
+        /// デバッグログを出力する。
+        /// </summary>
+        /// <param name="log">ログメッセージ</param>
+        /// <param name="file">呼び出し元のファイル名</param>
+        /// <param name="line">呼び出し元の行番号</param>
+        /// <param name="member">呼び出し元のメンバー名(メソッド・プロパティ・イベント名等)</param>
+        internal static void DebugLog(string log, 
+                [CallerFilePath] string file = "", 
+                [CallerLineNumber] int line = 0, 
+                [CallerMemberName] string member = "") {
+            System.Diagnostics.Debug.WriteLine(string.Format("[{0}][{1}][{2}]{3}", file, line, member, log));
         }
         #endregion
     }
