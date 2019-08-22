38f6ba8453c74a8e5e2843ae0f2b35fb07ad721b
OSN-DEV <osndev1973@gmail.com>
2019-08-22 22:02:35 +0900

    AppDataの実装

--- Data/AppData.cs
+++ Data/AppData.cs
@@ -1,36 +1,42 @@
 ﻿using System.Collections.Generic;
+using MyLib.Data;
+using MyNameList.Util;
 
 namespace MyNameList.Data {
     /// <summary>
     /// アプリケーションデータ。
     /// </summary>
-    internal class AppData : MyLib.Data.AppDataBase<AppData>{
+    public class AppData : AppDataBase<AppData> {
+
+        #region Declaration
+        private static readonly string FileName = "app.data";
+        #endregion
 
         #region Public Property
         /// <summary>
         /// 最後にアプリを表示していた時のウィンドウのサイズ(幅)。
         /// </summary>
-        internal double WindowSizeW { set; get; } = -1;
+        public double WindowSizeW { set; get; } = -1;
 
         /// <summary>
         /// 最後にアプリを表示していた時のウィンドウのサイズ(高さ)。
         /// </summary>
-        internal double WindowSizeH { set; get; } = -1;
+        public double WindowSizeH { set; get; } = -1;
 
         /// <summary>
         /// 最後にアプリを表示していた時のウィンドウの位置(X座標)。
         /// </summary>
-        internal double WindowPosX { set; get; } = -1;
+        public double WindowPosX { set; get; } = -1;
 
         /// <summary>
         /// 最後にアプリを表示していた時のウィンドウの位置(Y座標)。
         /// </summary>
-        internal double WindowPosY { set; get; } = -1;
+        public double WindowPosY { set; get; } = -1;
 
         /// <summary>
         /// 最近使ったファイルのリスト。
         /// </summary>
-        internal List<string> RecentFiles { set; get; } = new List<string>();
+        public List<string> RecentFiles { set; get; } = new List<string>();
         #endregion
 
         #region Public Method
@@ -39,14 +45,18 @@ namespace MyNameList.Data {
         /// </summary>
         /// <returns>AppDataのインスタンス</returns>
         internal static AppData GetInstance() {
-            return null;
+            if (null == AppDataBase<AppData>._instance) {
+                return AppDataBase<AppData>.GetInstanceBase(AppCommon.GetAppPath() + FileName);
+            } else {
+                return AppDataBase<AppData>.GetInstanceBase();
+            }
         }
 
         /// <summary>
         /// AppDataの情報を保存する。
         /// </summary>
         internal void Save() {
-
+            base.SaveToXml();
         }
         #endregion
     }
---
+++ Test/AppDataTest.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MyNameList.Data;
+
+namespace MyNameList.Test {
+    /// <summary>
+    /// AppDataTest.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AppDataTest : Window {
+        public AppDataTest() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var instance = AppData.GetInstance();
+            instance.WindowPosX = 1.0;
+            instance.Save();
+        }
+    }
+}
